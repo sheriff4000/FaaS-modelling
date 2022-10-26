4c158d704d0b59899ba0090efcba2898dd2090ad
Anthony Field <t.field@imperial.ac.uk>
2022-10-26 11:31:49 +0100

    Tidy up Cantral server model.

--- src/main/java/examples/CentralServer.java
+++ src/main/java/examples/CentralServer.java
@@ -15,13 +15,13 @@ public class CentralServer extends Sim {
 
     void runSim() {
         Network.initialise();
-        Delay cpuTime = new Delay(new Samplers.Exp(1 / 0.005));
+        Delay cpuTime   = new Delay(new Samplers.Exp(1 / 0.005));
         Delay disk1Time = new Delay(new Samplers.Exp(1 / 0.03));
         Delay disk2Time = new Delay(new Samplers.Exp(1 / 0.027));
 
         Source source = new Source("Source", new Samplers.Exp(lambda));
 
-        QueueingNode cpu = new QueueingNode("CPU", cpuTime, 1);
+        QueueingNode cpu   = new QueueingNode("CPU", cpuTime, 1);
         QueueingNode disk1 = new QueueingNode("Disk 1", disk1Time, 1);
         QueueingNode disk2 = new QueueingNode("Disk 2", disk2Time, 1);
 
@@ -29,15 +29,14 @@ public class CentralServer extends Sim {
 
         double[] routingProbs = {1.0 / 121.0, 70.0 / 121.0, 50.0 / 121.0};
         ProbabilisticBranch cpuOutputLink
-                = new ProbabilisticBranch(routingProbs, new Node[]{sink, disk1, disk2});
+           = new ProbabilisticBranch(routingProbs, new Node[]{sink, disk1, disk2});
 
         source.setLink(new Link(cpu));
         cpu.setLink(cpuOutputLink);
         disk1.setLink(new Link(cpu));
         disk2.setLink(new Link(cpu));
-        System.out.println("Hello");
-        simulate();
 
+        simulate();
         Network.logResults();
     }
 

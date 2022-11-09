73e8df2ecaa635ce822b76a31940db4ae39e5438
sheriff4000 <sheriff4000@gmail.com>
2022-11-09 16:51:49 +0000

    arrivals redone to use bernoulli split

--- src/main/java/mywork/FaaS.java
+++ src/main/java/mywork/FaaS.java
@@ -15,18 +15,26 @@ class FaaS extends Sim {
     // throughout the class
     int F = 10861;
     int M = 40;
+    float time;
+    Samplers.DiscreteEmpiricalSampler splitter;
+    DistributionSampler arrivals;
+    container_f[] container;
     f[] functions  = new f[10862];
 
     public class f{
         int S_time;
         int id;
         int invocations;
+        DistributionSampler service_time;
+        DistributionSampler request_time;
 
         public f(int id, int s, int i){
             System.out.println("initialising");
             this.id = id;
             this.S_time = s;
             this.invocations = i;
+            this.service_time = new Samplers.Exp(1/s);
+            this.request_time = new Samplers.Exp(i/30 / 24 / 60 / 60); // arrival rate in invocations/second
         }
 
 
@@ -34,12 +42,11 @@ class FaaS extends Sim {
 
     public class container_f{
         int id;
-        float last_called;
+        double last_called;
         boolean busy;
-
-        public container_f(int id, float last_called, boolean busy){
+        public container_f(int id, double last_called, boolean busy){
             this.id = id;
-            this.last_called = last_called;
+            this.last_called = last_called; // how long since its been called
             this.busy = busy;
         }
     }
@@ -74,12 +81,12 @@ class FaaS extends Sim {
     boolean[] busy = new boolean[40];
     float t;
 
-    DistributionSampler service_time = new Samplers.Exp(1.0 / 5);
-    DistributionSampler request_time = new Samplers.Exp(1.0 / 100);
+
 
     // The constructor will typically have configuration/model parameters
     public FaaS(int param) {
         // Setup array of all functions - containing service time and invocations;
+        time = 0;
         String file = "/Users/sherifagbabiaka/Documents/uni-3/Simulation and Modelling/part-i-code-CW/src/main/java/mywork/trace-final.csv";
         try {
             read_csv(file);
@@ -88,18 +95,40 @@ class FaaS extends Sim {
             e.printStackTrace();
         }
         //initialise container
-        container_f[] container = new container_f[40];
-        for(int i = 0; i < 40; i++){
+        container = new container_f[40];
+        for(int i = 1; i < 41; i++){
             container_f tmp = new container_f(i, 0, false);
-            container[i] = tmp;
+            container[i-1] = tmp;
         }
+
+        //combine arrival processes into one poisson process
+
+        float lambda = 0;
+        float[] lambdas = new float[F+1];
+        lambdas[0] = 0;
+
+        //sum arrival rates to create a single process
+        for(int i = 1; i < F+1; i++){
+            lambdas[i] = functions[i].invocations / 30 / 24 / 60 / 60;
+            lambda += lambdas[i];
+        }
+        //get probabilities for each rate
+        double[] ps = new double[F];
+        for (int i = 1; i < F+1; i++) {
+            ps[i] = lambdas[i] / lambda;
+        }
+        // Bernoulli splitting
+        splitter = new Samplers.DiscreteEmpiricalSampler(ps);
+        arrivals = new Samplers.Exp(lambda);
+
+
 
         //System.out.println(functions[10861].S_time); // 74?
 
 
         //this.something = param;
         // Schedule the initial event(s), e.g. to invoke E at time 10.0:
-        schedule(new E(10.0));
+        //schedule(new E(10.0));
         // To run the simulation do:
         simulate();
     }
@@ -123,15 +152,51 @@ class FaaS extends Sim {
 
     class request extends Event {
         // need to keep track of IDs
-        public request(double t) {
+        int id;
+        public request(double t, int id) {
             super(t);
+            this.id = id;
         }
 
         public void invoke() {
             //schedule next request
+
+            schedule(new request(now() + arrivals.next(), (int) splitter.next()));
             // check whether f is in container and available - determines whether request lost (busy = true)
-            // schedule new request
-            schedule(new E(now() + 10.0));
+            boolean in_container = false;
+
+            int max_idx = 0;
+            double longest_call = 0;
+
+            for(int i = 0; i < 40; i++){
+                //determine least recently called function
+                if(now() - container[i].last_called > longest_call){
+                    if(!container[i].busy){
+                        max_idx = i;
+                        longest_call = now() - container[i].last_called;
+                    }
+                }
+
+                //find function in container
+                if(container[i].id == id){
+                    in_container = true;
+                    if(!container[i].busy){
+                        //schedule completion
+                        container[i].last_called = now();
+                    }else{
+                        //throw away request i.e. do nothing
+                    }
+                }
+            }
+
+            if(!in_container){
+                //remove container[max_idx] replace with new func
+                container[max_idx] = new container_f(id, now(), true);
+                //schedule completion
+
+            }
+
+
         }
     }
 
